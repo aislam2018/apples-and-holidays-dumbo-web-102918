5a6ed210c97db18125e8e0e1482c00c7b9041e0c
anik1606 <anik1606@gmail.com>
2018-10-16 19:35:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.capitalize}:"
     holidays.each do |holiday, supply| 
-      puts " #{holiday.to_s.split("_").map { 
+      puts "  #{holiday.to_s.split("_").map { 
       |x| x.capitalize }.join(" ")}: #{supply.join(", ") }"
       
     end
